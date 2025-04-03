585a2575b67f166ed28d7643dedde8c65b075c9c
Ayan-Kashif <mrayan.buiz@gmail.com>
2025-04-03 13:39:56 +0500

    Update page.js

--- app/Favourites/page.js
+++ app/Favourites/page.js
@@ -16,7 +16,7 @@ export default function FavoritesPage() {
         <>
         <Navbar/>
       
-        <div className="min-h-screen mt-16 bg-gray-100   p-6">
+        <div className="min-h-screen text-black mt-16 bg-gray-100   p-6">
             <h1 className="text-3xl  font-bold text-center mb-6">Your Favorites</h1>
             {favorites.length > 0 ? (
                 <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
